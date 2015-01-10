10570b83155faede65e03f2aeaf69a7af53929fb
Andrew Reitz <aj.reitz@gmail.com>
2015-01-10 14:15:50 -0600

    Adding missing dependencies.

--- spock-android/src/main/java/com/andrewreitz/spock/AndroidSpockRunner.java
+++ spock-android/src/main/java/com/andrewreitz/spock/AndroidSpockRunner.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.andrewreitz.spock.test.runner;
+package com.andrewreitz.spock;
 
 import android.app.Activity;
 import android.app.Application;
