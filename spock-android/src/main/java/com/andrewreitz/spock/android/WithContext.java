cde99333e7976473fbcd6fc632cb856a66e8efce
Andrew Reitz <aj.reitz@gmail.com>
2015-02-08 22:07:01 -0600

    Added javadoc

--- spock-android/src/main/java/com/andrewreitz/spock/android/WithContext.java
+++ spock-android/src/main/java/com/andrewreitz/spock/android/WithContext.java
@@ -7,6 +7,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.spockframework.runtime.extension.ExtensionAnnotation;
 
+/**
+ * Use to get the context in your tests.
+ * {@code @WithContext def context}
+ *
+ * @author Andrew Reitz
+ * @since 1.1
+ */
 @Retention(RetentionPolicy.RUNTIME) @Target(ElementType.FIELD)
 @ExtensionAnnotation(WithContextExtension.class)
 public @interface WithContext {
