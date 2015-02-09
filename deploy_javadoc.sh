67df51f7f28cf3ae1a72b08b8dd98b60f9411e05
Andrew Reitz <aj.reitz@gmail.com>
2015-02-08 23:03:14 -0600

    added deploy javadoc script

---
+++ deploy_javadoc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -ex
+
+REPO="git@github.com:pieces029/android-spock.git"
+DIR=temp-clone
+
+# Delete any existing temporary website clone
+rm -rf $DIR
+
+# Clone the current repo into temp folder
+git clone $REPO $DIR
+
+# Move working directory into temp folder
+cd $DIR
+
+# Checkout and track the gh-pages branch
+git checkout -t origin/gh-pages
+
+# Delete everything
+rm -rf *
+
+# Copy everything from javadocs folder
+cp -r ../spock-android/build/docs/javadoc/* .
+
+# Stage all files in git and create a commit
+git add .
+git add -u
+git commit -m "Website at $(date)"
+
+# Push the new files up to GitHub
+git push origin gh-pages
+
+# Delete our temp folder
+cd ..
+rm -rf $DIR
